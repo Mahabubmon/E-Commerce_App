5ac1dbc21df7bfb0f1012c66bd039ef709dcf94c
Mahabubur Rahman <mahabubmon@gmail.com>
2023-10-16 19:49:49 +0600

    AddCategory_adding_form

--- src/views/Category/AddCategory.vue
+++ src/views/Category/AddCategory.vue
@@ -1,5 +1,21 @@
 <template>
     <div>Add a Category</div>
+    <div class="container">
+        <div class="row">
+            <div class="text-center">
+                <h3>Add Category</h3>
+            </div>
+        </div>
+        <div class="row">
+            <form action="">
+                <div class="form-group">
+                    <label for="">
+                        <input type="text" class="form-control">
+                    </label>
+                </div>
+            </form>
+        </div>
+    </div>
 </template>
 
 <script>
