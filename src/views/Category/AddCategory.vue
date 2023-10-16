5ebc3e1e5bd0b168fe9cb14ddee3935400eb2a05
Mahabubur Rahman <mahabubmon@gmail.com>
2023-10-16 19:57:32 +0600

    Adding_some_alignment

--- src/views/Category/AddCategory.vue
+++ src/views/Category/AddCategory.vue
@@ -6,8 +6,10 @@
                 <h3>Add Category</h3>
             </div>
         </div>
-        <div class="row">
-            <form action="">
+        <div class="row ">
+            <div class="col-3"></div>
+            <div class="col-6">
+                <form action="">
                 <div class="form-group">
                     <label for="">Category Name</label>
                         <input type="text" class="form-control">
@@ -20,8 +22,11 @@
                     <label for="">Category Image</label>
                         <input type="text" class="form-control">
                 </div>
-                <button type="submit" ></button>
+                <button type="submit" class="" >Submit</button>
             </form>
+            </div>
+            <div class="col-3"></div>
+            
         </div>
     </div>
 </template>
