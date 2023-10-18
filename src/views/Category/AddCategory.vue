231ed31b5a93f8d0e5352034ae1f9d60225a76e3
Mahabubur Rahman <mahabubmon@gmail.com>
2023-10-18 20:49:44 +0600

    v-model_adding

--- src/views/Category/AddCategory.vue
+++ src/views/Category/AddCategory.vue
@@ -3,8 +3,8 @@
 
     <div class="container">
         <div class="row">
-            <div class="text-center">
-                <h3><div> Add category</div></h3>
+            <div class=" col-12 text-center">
+                <h3 class="pt-3">Add category</h3>
             </div>
         </div>
         <div class="row">
@@ -13,17 +13,17 @@
                 <form>
                 <div class="form-group">
                     <label for="">Name</label>
-                    <input type="text" class="form-control">
+                    <input type="text" class="form-control" v-model ="categoryName">
                 </div>
                 <div class="form-group">
                     <label for="">Description</label>
-                    <input type="text" class="form-control">
+                    <textarea type="text" class="form-control" v-model ="description"/>
                 </div>
                 <div class="form-group">
                     <label for="">Image</label>
-                    <input type="text" class="form-control">
+                    <input type="text" class="form-control" v-model ="imageUrl">
                 </div>
-                <button type="submit" >Submit</button>
+                <button type="submit" class="btn btn-primary" >Submit</button>
             </form>
             </div>
             <div class="col-3"></div>
@@ -35,7 +35,13 @@
 
 <script>
 export default{
-    setup(){},
+    data(){
+        return {
+            categoryName:"",
+            description:"",
+            imageUrl:""
+        }
+    },
 
 }
 
