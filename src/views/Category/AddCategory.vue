296cfc19036e1b1d0620ee8cd7d031a03f803d67
Mahabubur Rahman <mahabubmon@gmail.com>
2023-10-18 20:31:59 +0600

    adding form group

--- src/views/Category/AddCategory.vue
+++ src/views/Category/AddCategory.vue
@@ -1,5 +1,20 @@
 <template>
-    <div> Add category</div>
+    
+
+    <div class="container">
+        <div class="row">
+            <div class="text-center">
+                <h3><div> Add category</div></h3>
+            </div>
+        </div>
+        <div class="row">
+            <form action="" class="form-group">
+                <div class="form-group">
+                    
+                </div>
+            </form>
+        </div>
+    </div>
 </template>
 
 
